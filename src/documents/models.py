32b311608659dbb2d482c4b4b7a62c5d76f9bd0f
Avirup Dasgupta <avirupdasgupta3628@gmail.com>
2026-02-07 14:16:01 +0530

    Database creation-and setting of migrations

---
+++ src/documents/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+# Create your models here.
+
+User = settings.AUTH_USER_MODEL #-> auth.user
+
+class Document(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE) #if user is deleted, delete all documents related to that user
+    title = models.CharField(max_length=255, default="Title")
+    content = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    active_at = models.DateTimeField(auto_now_add=False,auto_now=False, null=True) # when the document is active, this field will be updated with the current time
+    created_at = models.DateTimeField(auto_now_add=True) #db auto update this field when it's created
+    updared_at = models.DateTimeField(auto_now=True) # db auto update this field when it's updated
+
+    def save(self, *args, **kwargs):
+        if self.active and self.active_at is not None:
+            self.active_at = timezone.now()
+        else:
+            self.active_at = None
+        super().save(*args, **kwargs)
